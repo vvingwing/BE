b19e63dbbca4fe368da3bbe048fa12e95ddf16e5
controlZ <o_dahyun@gm.gist.ac.kr>
2023-04-30 06:19:56 +0900

    🐛 fix: solve cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: function (origin, callback) {
+      callback(null, origin);
+      // if (!origin || whitelist.indexOf(origin) !== -1) {
+      //   callback(null, origin);
+      // } else {
+      //   callback(new Error('Not allowed by CORS'));
+      // }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
